3ad57efd662854bac6a1cc9674878fdb61116e6a
syamcat <kdh1998kdh@gmail.com>
2025-04-08 09:16:19 +0900

    25314

--- 25314번: 코딩은 체육과목 입니다/Main.java
+++ 25314번: 코딩은 체육과목 입니다/Main.java
@@ -9,3 +9,27 @@
 /*   Solved: 2025/04/07 12:00:45 by kdh1998kdh    ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int inputNum;
+		int count;
+
+		inputNum = sc.nextInt();
+
+		count = inputNum / 4;
+
+		if (inputNum % 4 > 0)
+			count++;
+
+		while (count > 0) {
+			System.out.printf("long ");
+			count--;
+		}
+
+		System.out.printf("int\n");
+	}
+}
